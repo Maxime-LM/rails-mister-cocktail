36faec6321c044e66ad24877f2c6d918eea8e73c
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-05-31 17:13:44 +0200

    add uppload / seed img

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ rhum = Ingredient.create(name: "rhum")
 vin_blanc = Ingredient.create(name: "vin blanc")
 tequila = Ingredient.create(name: "tequila")
 
-Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Mojito",   photo: File.open(Rails.root.join('db/fixtures/cocktails/mojito.jpg')))
 
 Dose.create!(description: "6cl", cocktail: Cocktail.first, ingredient: rhum )
 Dose.create!(description: "3cl de jus", cocktail: Cocktail.first, ingredient: citron )
@@ -26,13 +26,13 @@ Dose.create!(description: "7 feuilles de menthe", cocktail: Cocktail.first, ingr
 Dose.create!(description: "remplir le reste du verre", cocktail: Cocktail.first, ingredient: eau_gazeuse )
 Dose.create!(description: "2 cl", cocktail: Cocktail.first, ingredient: sucre )
 
-lemontype = Cocktail.create!(name: "Lemon type")
+lemontype = Cocktail.create!(name: "Lemon type", photo: File.open(Rails.root.join('db/fixtures/cocktails/citronwine.jpg')))
 
 Dose.create!(description: "5cl", cocktail: lemontype, ingredient: vin_blanc )
 Dose.create!(description: "3cl", cocktail: lemontype, ingredient: citron )
 Dose.create!(description: "remplir le reste du verre", cocktail: lemontype, ingredient: eau_gazeuse )
 
-mocking = Cocktail.create!(name: "Mockingbird")
+mocking = Cocktail.create!(name: "Mockingbird", photo: File.open(Rails.root.join('db/fixtures/cocktails/mocki.jpg')))
 Dose.create!(description: "5cl", cocktail: mocking, ingredient: tequila )
 Dose.create!(description: "1cl", cocktail: mocking, ingredient: citron )
 
